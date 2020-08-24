467e336bee7e6f3bd04b539f2c1c52fc8540b553
Ankitha <ankinr09@gmail.com>
2020-08-24 10:27:20 -0400

    Add changes to get day one

--- data.py
+++ data.py
@@ -2,13 +2,13 @@ import os
 import requests
 import json
 
-def base():    
+def base(country):    
     data=""
-    req=requests.get("https://api.covid19api.com/countries")
+    req=requests.get("https://api.covid19api.com/dayone/country/{}/status/confirmed".format(country))
     data=json.loads(req.text)
     return data
 if __name__=="__main__":
-    print(base())
+    print(base("south-africa"))
 
 
 
