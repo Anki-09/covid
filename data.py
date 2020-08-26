2d44622747f1e522630f147b215699d49e5ea4d7
Anki-09 <70140224+Anki-09@users.noreply.github.com>
2020-08-26 12:57:28 +0530

    Feature/getdaytotal (#2)
    * Add change to enable getting total counts by country
    * Add changes to allow for country selection

--- data.py
+++ data.py
@@ -7,8 +7,17 @@ def base(country):
     req=requests.get("https://api.covid19api.com/dayone/country/{}/status/confirmed".format(country))
     data=json.loads(req.text)
     return data
+def get_total():
+    url="https://api.covid19api.com/total/dayone/country/south-africa/status/confirmed"
+    data={}
+    req=requests.get(url)
+    if req.status_code==200:
+        data=json.loads(req.text)
+    return data
 if __name__=="__main__":
-    print(base("south-africa"))
+    print(base("India"))
+    print(get_total())
+
 
 
 
